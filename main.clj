85a3db2001082e55d37ed1f2500360f4313c2c6a
Code <59381987+c-o-de@users.noreply.github.com>
2023-08-22 09:59:32 +0600

    Add files via upload

---
+++ main.clj
@@ -0,0 +1,9 @@
+;; This is a comment in Clojure. It's ignored by the Clojure interpreter and used for documentation.
+
+;; Define a function to greet a person
+(defn greet [name]
+  (println (str "Hello, " name "!")))
+
+;; Call the greet function with different names
+(greet "Alice")
+(greet "Bob")
